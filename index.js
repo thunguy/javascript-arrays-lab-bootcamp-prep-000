60a7362270fa34b23ee99f9fafb39c521c89875f
thunguy@me.com <thunguy@me.com>
2018-05-11 17:47:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,28 +1,2 @@
-const app = "I don't do much."
-var kittens = [
-  "Milo",
-  "Otis",
-  "Garfield",
-  ]
-  
-function destructivelyAppendKitten(name) {
-  return kittens.push(name);
-}
-  
-function destructivelyPrependKitten(name) {
-  return kittens.unshift(name);
-}
-  
-function destructivelyRemoveLastKitten() {
-  return kittens.pop();
-}
-  
-function destructivelyRemoveFirstKitten() {
-  return kittens.shift();
-}
-
-var appendKitten = function appendKitten(name)() {
-  return [name, ...kittens];
-}
 
 

a09da3b2deb2912480c5cb612ad58cba074318ab
thunguy@me.com <thunguy@me.com>
2018-05-11 18:08:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,11 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  return [...kittens, name];
+  return [...kittens, name]
+}
+
+function prependKitten(name) {
+  return [name, ...kittens]
 }
 
 
